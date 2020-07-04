8baf6d923a680117a9d5e5f8e7e7a52696a7b3f1
scissorsnatcher <ayntewc@gmail.com>
2020-07-04 03:16:03 +0300

    .

--- Source/Graph.java
+++ Source/Graph.java
@@ -50,9 +50,5 @@ public class Graph implements GraphInterface{
 		System.out.println(" что-то произошло");
 		
 	}
-	public static void main(String args[]) {
-		 Graph graph= new Graph();
-		 graph.readFromFile();
-	}
 
 }
