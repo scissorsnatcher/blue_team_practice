e243af1da3680748dd8f1c2bbd78f1403cb66809
scissorsnatcher <ayntewc@gmail.com>
2020-07-06 02:55:30 +0300

    .

--- Source/IChangable.java
+++
@@ -1,8 +0,0 @@
-public interface IChangable {
-
-	public abstract void AddVertice();
-	public abstract void CommonVertices();
-	public abstract void Boruvka();
-	public abstract void DeleteVertice();
-	
-}
--- Source/ILoadable.java
+++
@@ -1,7 +0,0 @@
-public interface ILoadable {
-	
-	public void readFromFile();
-	public void changeGraph();
-	public void generateGraph();
-	
-}
